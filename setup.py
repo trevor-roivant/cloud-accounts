ace854b12018a602e91093d1c49057a72de99064
trevor-roivant <trevor.cardwell@sumitovant.com>
2023-10-23 01:18:11 -0400

    updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox-cloud-accounts',
-    version='0.5',
+    version='0.6',
     description='Keep inventory of cloud accountsn',
     install_requires=[],
     packages=find_packages(),
